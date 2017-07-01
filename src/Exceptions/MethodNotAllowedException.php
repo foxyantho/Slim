d3d618f6f091084f22a989e3e24c784c246e7d2e
rivetchip <me@spidery.fr>
2017-07-01 22:58:04 +0200

    fix redeclared variables

--- src/Exceptions/MethodNotAllowedException.php
+++ src/Exceptions/MethodNotAllowedException.php
@@ -16,9 +16,6 @@ use Slim\Http\Interfaces\ResponseInterface as Response;
 class MethodNotAllowedException extends SlimException
 {
 
-    protected $allowedMethods;
-
-
     /**
      * HTTP methods allowed
      *
