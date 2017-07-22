e39c81cd98b359cbf9e35f45fc4655b8d6978fed
rivetchip <me@spidery.fr>
2017-07-22 11:06:01 +0200

    mock

--- src/Http/Environment.php
+++ src/Http/Environment.php
@@ -23,6 +23,33 @@ class Environment extends Collection implements EnvironmentInterface
 {
 
     /**
+     * Create mock environment
+     *
+     * @param  array $userSettings Array of custom environment keys and values
+     * @return self
+     */
+    public static function mock( array $userSettings = [] )
+    {
+        return array_merge([
+            'SERVER_PROTOCOL'      => 'HTTP/1.1',
+            'REQUEST_METHOD'       => 'GET',
+            'SCRIPT_NAME'          => '',
+            'REQUEST_URI'          => '/',
+            'QUERY_STRING'         => '',
+            'SERVER_NAME'          => 'localhost',
+            'SERVER_PORT'          => 80,
+            'HTTP_HOST'            => 'localhost',
+            'HTTP_ACCEPT'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'HTTP_ACCEPT_LANGUAGE' => 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+            'HTTP_USER_AGENT'      => 'Slim Framework',
+            'REMOTE_ADDR'          => '127.0.0.1',
+            'REQUEST_TIME'         => time(),
+            'REQUEST_TIME_FLOAT'   => microtime(true),
+        ], $userSettings);
+    }
+
+
+    /**
      * Normalize key "key_subkey" ( lowercase )
      * 
      * @param  mixed $key
